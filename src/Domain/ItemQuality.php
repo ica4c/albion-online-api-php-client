9af43860e24f091c6f464e63926cde2da5cef4f9
Ruslan Gataullin <fisher7410@yandex.ru>
2020-08-11 05:43:41 +0500

    Updated models to the latest states

--- src/Domain/ItemQuality.php
+++ src/Domain/ItemQuality.php
@@ -2,36 +2,13 @@
 
 namespace Albion\OnlineDataProject\Domain;
 
-class ItemQuality
+use Albion\OnlineDataProject\Foundation\DataTypes\Enumerable;
+
+class ItemQuality extends Enumerable
 {
     const NORMAL = 1;
     const GOOD = 2;
     const OUTSTANDING = 3;
     const EXCELLENT = 4;
     const MASTERPIECE = 5;
-
-    /**
-     * Location constructor.
-     *
-     * @param \Albion\OnlineDataProject\Models\string $type
-     */
-    public function __construct(string $type)
-    {
-        switch ($type) {
-            case static::FORT_STERLING:
-            case static::THETFORD:
-            case static::BRIDGEWATHCH:
-            case static::LYMHURST:
-            case static::MARTLOCK:
-            case static::KAERLEON:
-                break;
-
-            default:
-                throw new InvalidArgumentException('Unsupported enum type');
-        }
-    }
-
-    public static function of($type) {
-        return new static($type);
-    }
 }
