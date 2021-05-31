3c9553548c0cfa113d3436b798211e642d9d856c
Ruslan Gataullin <fisher7410@yandex.ru>
2021-05-31 16:35:27 +0500

    Added retry middleware

--- src/Infrastructure/GameInfo/AbstractClient.php
+++ src/Infrastructure/GameInfo/AbstractClient.php
@@ -3,6 +3,8 @@
 namespace Albion\API\Infrastructure\GameInfo;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleRetry\GuzzleRetryMiddleware;
 
 abstract class AbstractClient
 {
@@ -16,9 +18,15 @@ abstract class AbstractClient
      */
     public function __construct()
     {
+        $stack = HandlerStack::create();
+        $stack->push(GuzzleRetryMiddleware::factory());
+
         $this->httpClient = new Client(
             [
                 'base_uri' => 'https://gameinfo.albiononline.com/api/gameinfo/',
+                'handler' => $stack,
+                'retry_on_timeout' => true,
+                'max_retry_attempts' => 3,
                 'connect_timeout' => 5,
                 'timeout' => 90
             ]
